f147f187c3aa11c05ef9992b6e3e089276a5e1d1
namskram <wildbiduuf@gmail.com>
2023-01-18 23:11:50 -0800

    fixed MainActivity

--- app/src/main/java/edu/ucsd/cse110/lab2/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/lab2/MainActivity.java
@@ -136,7 +136,7 @@ public class MainActivity extends AppCompatActivity {
         // Clear the pending operation and update the display.
         pendingOp = Optional.empty();
         displayStr = String.valueOf(result);
-        Utilities.trimDisplayStr(this.displayStr);
+        Utilities.trimDisplayStr(displayStr);
         updateDisplay();
     }
 
